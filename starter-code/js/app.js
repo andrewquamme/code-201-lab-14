9b940fbd0c8572a92d7dd7f3bf183250356fad68
Harris <harris.richardson87@gmail.com>
2018-10-25 15:50:41 -0700

    harrissruff

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -14,7 +14,10 @@ Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
 };
 
-Cart.prototype.removeItem = function(item) {
+Cart.prototype.removeItem = removeItemFromCart(event) {
+  if (event.target.contains === 'Remove'){
+  }
+
   // TODO: Fill in this instance method to remove one item from the cart.
   // Note: You will have to decide what kind of parameter to pass in here!
 };
